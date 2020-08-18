df3cbb23080a909043245b11233069d2916cf79f
“msdzifamensah” <msdzifamensah@gmail.com>
2020-08-18 06:22:20 +0000

    new change to first dart program

--- Dart/dart1/bin/dart1.dart
+++ Dart/dart1/bin/dart1.dart
@@ -6,7 +6,12 @@ void main() {
   print('The number of rabbits increases as:\n');
 
   for (var years = 0; years <= 10; years++) {
-    n = (2 * pow(e, log(15) * years)).round().toInt();
+    n = calculateRabbit(n, years);
     print('After $years years:\t $n animals');
   }
+}
+
+int calculateRabbit(int n, int years) {
+  n = (2 * pow(e, log(15) * years)).round().toInt();
+  return n;
 }
