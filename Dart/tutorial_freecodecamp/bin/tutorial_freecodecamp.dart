49cc5b42715a493dc14acab516e9228ed1f017f9
“msdzifamensah” <msdzifamensah@gmail.com>
2020-09-01 06:38:52 +0000

    commit

---
+++ Dart/tutorial_freecodecamp/bin/tutorial_freecodecamp.dart
@@ -0,0 +1,5 @@
+import 'package:tutorial_freecodecamp/tutorial_freecodecamp.dart' as tutorial_freecodecamp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tutorial_freecodecamp.calculate()}!');
+}
---
+++ Dart/tutorial_freecodecamp/test/tutorial_freecodecamp_test.dart
@@ -0,0 +1,8 @@
+import 'package:tutorial_freecodecamp/tutorial_freecodecamp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
